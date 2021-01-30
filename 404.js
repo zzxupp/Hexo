a8a5e48c5625d044eecbf9aeb2e7afcbebe2a460
zzxupp <75357370+zzxupp@users.noreply.github.com>
2021-01-30 17:13:24 +0800

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,19 +1,4 @@
-import Error from 'next/error'
-
-export async function getServerSideProps() {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const errorCode = res.ok ? false : res.statusCode
-  const json = await res.json()
-
-  return {
-    props: { errorCode, stars: json.stargazers_count },
-  }
-}
-
-export default function Page({ errorCode, stars }) {
-  if (errorCode) {
-    return <Error statusCode={errorCode} />
-  }
-
-  return <div>Next stars: {stars}</div>
+// pages/404.js
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>
 }
